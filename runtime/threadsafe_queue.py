7673105fd13c4525d04146af35d919ee102e3ead
mcao2 <mcao2@andrew.cmu.edu>
2020-04-30 21:17:33 -0400

    add timing to the queue

--- runtime/threadsafe_queue.py
+++ runtime/threadsafe_queue.py
@@ -2,10 +2,13 @@
 # Licensed under the MIT license.
 
 import threading
+import time
 
 """
 Implementation of a thread-safe queue with one producer and one consumer.
 """
+
+
 class Queue:
     def __init__(self):
         self.queue = []
@@ -13,6 +16,7 @@ class Queue:
 
     def add(self, tensor):
         self.cv.acquire()
+        self.start_time = time.time()
         self.queue.append(tensor)
         self.cv.notify()
         self.cv.release()
@@ -22,5 +26,12 @@ class Queue:
         while len(self.queue) == 0:
             self.cv.wait()
         tensor = self.queue.pop(0)
+        self.end_time = time.time()
         self.cv.release()
         return tensor
+
+    def get_waittime(self):
+        try:
+            return (self.end_time - self.start_time) * 1000.0
+        except BaseException:
+            return -1
